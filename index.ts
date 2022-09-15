37248774b9bce9a2e072502fb6e0c56e0e4386b6
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2022-09-15 23:11:36 +0300

    Better logging

--- index.ts
+++ index.ts
@@ -10,30 +10,36 @@ app.get('/*', async (req, res) => {
   const protocol = req.header('x-forwarded-proto') || req.protocol;
   const remoteUrl = new URL(req.url.toLowerCase(), `${protocol}://developers.buymeacoffee.com/`);
 
-  console.log('Received request:', { url: req.url, remoteUrl: remoteUrl.href });
-
   // Make sure the URL points to an API endpoint
   if (!remoteUrl.pathname.startsWith('/api/v1')) {
     res.status(400).end('Path must start with /api/v1');
     return;
   }
 
+  console.log('Received request:', {
+    remoteUrl: remoteUrl.href,
+    headers: req.headers,
+    data: req.body
+  });
+
   // Fetch cached response
   const cachedResponse = await getCacheItem(remoteUrl.pathname);
   if (cachedResponse) {
     res.status(200).end(cachedResponse);
-    console.log('Served cached response');
+
+    console.log('Served cached response', {
+      remoteUrl: remoteUrl.href,
+      data: cachedResponse
+    });
+
     return;
   }
 
   // Fetch remote response
   const response = await axios.get(remoteUrl.href, {
-    // Copy headers over from the request
-    headers: Object.fromEntries(
-      Object.entries(req.headers)
-        .map(([key, value]) => (typeof value !== 'object' ? [key, value] : [key, value[0]]))
-        .filter(([, value]) => !!value)
-    ),
+    headers: {
+      authorization: req.header('authorization') || ''
+    },
     validateStatus: () => true
   });
 
@@ -42,13 +48,14 @@ app.get('/*', async (req, res) => {
     await setCacheItem(remoteUrl.pathname, response.data);
   }
 
-  // Proxy headers
-  for (const [key, value] of Object.entries(response.headers)) {
-    res.setHeader(key, value);
-  }
-
   res.status(response.status).end(response.data);
-  console.log('Served live response:', { status: response.status });
+
+  console.log('Served live response:', {
+    remoteUrl: remoteUrl.href,
+    status: response.status,
+    headers: response.headers,
+    data: response.data
+  });
 });
 
 app.listen(port, () => {
